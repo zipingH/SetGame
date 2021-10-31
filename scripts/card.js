0850b7e8fa06470985a84004dab197e730642062
Ziping Huang <zipingh777@gmail.com>
2021-10-31 00:12:17 +0000

    Added file input and output.

---
+++ scripts/card.js
@@ -0,0 +1,8 @@
+export default class Card{
+  construtor(color, symbol, shading, number){
+    this.color = color;
+    this.symbol = symbol;
+    this.shading = shading;
+    this.number = number;
+  }
+}
---
+++ scripts/fileInput.js
@@ -0,0 +1,55 @@
+
+exports.fileInput = function(){
+  //input file
+  var fileInput = document.getElementById("fileInput");
+  var fileOutput = document.getElementById("fileOutput");
+
+  fileInput.onchange = function(){
+    var file = this.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function(event){
+    //read entire file
+    // console.log(this.result);
+
+   //read file line by line.
+      var lines = this.result.split('\n');
+      var numberOfCards = lines[0];
+      for(var line = 1; line < lines.length; line++){
+        console.log(lines[line]);
+      
+      //output
+        fileOutput.textContent = event.target.result;
+      }
+    // console.log(numberOfCards);
+    }
+    reader.readAsText(file);
+  };
+}
+
+// //input file
+// var fileInput = document.getElementById("fileInput");
+// var fileOutput = document.getElementById("fileOutput");
+
+// fileInput.onchange = function(){
+//   var file = this.files[0];
+//   var reader = new FileReader();
+
+//   reader.onload = function(event){
+//     //read entire file
+//     // console.log(this.result);
+
+//    //read file line by line.
+//     var lines = this.result.split('\n');
+//     var numberOfCards = lines[0];
+//     for(var line = 1; line < lines.length; line++){
+//       console.log(lines[line]);
+      
+//       //output
+//       fileOutput.textContent = event.target.result;
+//     }
+//     // console.log(numberOfCards);
+//   }
+//   reader.readAsText(file);
+// };
+
